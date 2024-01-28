adc854df7cab4f8280466515a7874f363985850c
fixirorteam <fixirorinc@gamil.com>
2024-01-28 00:26:32 -0800

    version 1.2

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const port = process.env.PORT_SERVER || 6000
 
 app.use(cors())
 
-app.use(EspRoutes)
-app.use(AuthEsp)
+app.use('/', EspRoutes)
+app.use('/', AuthEsp)
 
 app.listen(port , () => {
     console.log(`Server running on ${port}`);
